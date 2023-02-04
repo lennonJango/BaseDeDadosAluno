699fb1db085adcd8e1d169b41b93047083a2a4ae
unknown <lennonjango@gmail.com>
2023-02-04 11:24:19 +0200

    Atualização do Ficheiro

--- src/est/ViewEstudante.java
+++ src/est/ViewEstudante.java
@@ -27,7 +27,7 @@ public class ViewEstudante extends JFrame implements ActionListener {
 	private JTextField textNome;
 	private JTextField textEndereco;
 	private JTable table;
-	JButton btnInserir, btnList,btnUpdate,btnRemove;
+	JButton btnInserir, btnList, btnUpdate, btnRemove;
 	private JTextField textNivelAcademico;
 	private JLabel lblNewLabel_3;
 
@@ -58,87 +58,79 @@ public class ViewEstudante extends JFrame implements ActionListener {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBounds(36, 23, 322, 320);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		textCodigo = new JTextField();
 		textCodigo.setBounds(10, 53, 111, 23);
 		panel.add(textCodigo);
 		textCodigo.setColumns(10);
-		
+
 		textNome = new JTextField();
 		textNome.setColumns(10);
 		textNome.setBounds(10, 135, 275, 32);
 		panel.add(textNome);
-		
+
 		textEndereco = new JTextField();
 		textEndereco.setColumns(10);
 		textEndereco.setBounds(10, 217, 275, 38);
 		panel.add(textEndereco);
-		
+
 		JLabel lblNewLabel = new JLabel("Codigo do estudante");
 		lblNewLabel.setBounds(10, 28, 153, 14);
 		panel.add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Nome do estudante");
 		lblNewLabel_1.setBounds(10, 110, 129, 14);
 		panel.add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Endereço do estudante");
 		lblNewLabel_2.setBounds(10, 186, 129, 20);
 		panel.add(lblNewLabel_2);
-		
+
 		textNivelAcademico = new JTextField();
 		textNivelAcademico.setBounds(177, 53, 96, 23);
 		panel.add(textNivelAcademico);
 		textNivelAcademico.setColumns(10);
-		
+
 		lblNewLabel_3 = new JLabel("Nivel Academico");
 		lblNewLabel_3.setBounds(188, 28, 97, 14);
 		panel.add(lblNewLabel_3);
-		
-		 btnInserir = new JButton("Insert");
+
+		btnInserir = new JButton("Insert");
 		btnInserir.setVerticalAlignment(SwingConstants.BOTTOM);
 		btnInserir.setBounds(42, 403, 89, 23);
 		btnInserir.addActionListener(this);
 		contentPane.add(btnInserir);
-		
-		 btnList = new JButton("List");
+
+		btnList = new JButton("List");
 		btnList.setBounds(197, 403, 89, 23);
 		contentPane.add(btnList);
-		 btnList.addActionListener(this);
-		
-		 btnUpdate = new JButton("Update");
+		btnList.addActionListener(this);
+
+		btnUpdate = new JButton("Update");
 		btnUpdate.setBounds(42, 481, 89, 23);
 		contentPane.add(btnUpdate);
 		btnUpdate.addActionListener(this);
-		
-		 btnRemove = new JButton("Remove");
+
+		btnRemove = new JButton("Remove");
 		btnRemove.setBounds(197, 481, 89, 23);
 		contentPane.add(btnRemove);
 		btnRemove.addActionListener(this);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(721, 530, -328, -498);
 		contentPane.add(scrollPane);
-		
+
 		table = new JTable();
 		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-			},
-			new String[] {
-				"Nivel Academico", "Codigo", "Nome", " Endere\u00E7o"
-			}
-		) {
-			Class[] columnTypes = new Class[] {
-				Integer.class, Object.class, Object.class, Object.class
-			};
+				new Object[][] { { null, null, null, null }, { null, null, null, null }, { null, null, null, null }, },
+				new String[] { "Nivel Academico", "Codigo", "Nome", " Endere\u00E7o" }) {
+			Class[] columnTypes = new Class[] { Integer.class, Object.class, Object.class, Object.class };
+
 			public Class getColumnClass(int columnIndex) {
 				return columnTypes[columnIndex];
 			}
@@ -153,17 +145,32 @@ public class ViewEstudante extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		String nome = textNome.getText();
+		int codigo = Integer.parseInt(textCodigo.getText());
+		String endereco = textEndereco.getText();
+		int anoLectivo = Integer.parseInt(textNivelAcademico.getText());
+
+		Controle cc = new Controle();
+
+		if (e.getSource() == btnInserir) {
+
+			cc.adicionarEstudante(codigo, nome, endereco, anoLectivo);
+
+		}
+
+		if (e.getSource() == btnList) {
+			cc.listar();
+
+		}
+
+		if (e.getSource() == btnUpdate) {
+
+			cc.Actualizar(codigo, nome, endereco, anoLectivo);
+		}
 		
-		 if(e.getSource()==btnInserir) {
-			 String nome = textNome.getText();
-			 int codigo = Integer.parseInt(textCodigo.getText());
-			 String endereco = textEndereco.getText();
-			  int anoLectivo = Integer.parseInt(textNivelAcademico.getText());
-			 
-			 Controle cc = new Controle();
-			 cc.adicionarEstudante(codigo, nome, endereco,anoLectivo);
-			 
-		 }
-		
+		if(e.getSource()==btnRemove) {
+			cc.remover(codigo);
+		}
+
 	}
 }
